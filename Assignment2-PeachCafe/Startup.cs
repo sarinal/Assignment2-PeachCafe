ea52b625a0aebf53b99405c706345f5934d10e26
sarinal <sarina-luu@hotmail.com>
2016-11-19 21:05:32 -0500

    Add project files.

---
+++ Assignment2-PeachCafe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment2_PeachCafe.Startup))]
+namespace Assignment2_PeachCafe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
